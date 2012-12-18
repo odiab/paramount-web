14ccbff7a829f6a916b5fff6defa565c9a09bd9d
Omar Diab <odiab@stanford.edu>
2012-12-18 13:24:46 -0800

    added google web font, bigger line height

--- templates/head.php
+++ templates/head.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
   <title>Paramount Medical Research and Consulting</title>
   <link rel="stylesheet" href="/style.css" media="screen" />
 </head>
