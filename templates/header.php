93588f2a3120f4147d49efeb11ec8b868705636e
Omar Diab <o.s.diab@gmail.com>
2012-12-18 11:01:24 -0800

    cleaned up style some more, gonna fix redundancy in next commit

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<div id="header">
+  <div class="headerlogo">
+    <table class="cellLink" width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height = 250px><a href="/index.php"></a></td>
+      </tr>
+    </table>
+  </div>
+
+  <? include('navigation.php'); ?>
+</div>
---
+++ templates/sidebar.php
@@ -0,0 +1,3 @@
+<div class="sidebar1">
+  <p>This website is still under construction.</p>
+</div>
