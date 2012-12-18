be27d8e12074f9ca78ecc80572b237da7d190fa5
Omar Diab <o.s.diab@gmail.com>
2012-12-18 13:08:34 -0800

    forgot a few files

--- dir/About/staff.php
+++ dir/About/staff.php
@@ -1,53 +1,32 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<title>Paramount Medical Research and Consulting</title>
-<link rel="stylesheet" type="text/css" href="../../style.css" />
-</head>
+<?php
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  include($root . '/templates/head.php');
+?>
 
 <body>
 
-<table width="100%" height="50px" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td></td>
-  </tr>
-</table>
-
 <div class="container">
- 	<div class="headerlogo">
-      <table class="cellLink" width="100%" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-          <td height = 200px><a href="../../index.php"></a></td>
-        </tr>
-      </table>
-    </div>
-<!--navigation bar-->
-<? readfile("../../navigation.html"); ?>
-<!--navigation bar-->
-<div id="bodybox">
-  <div class="sidebar1">
-<p>This website is still under construction.</p>
+  <?php include($root . '/templates/header.php'); ?>
+
+  <div id="bodybox">
+    <div id="content">
+      <h1>Meet Our Doctors</h1>
+      <hr align=left noshade="noshade" size=1 />
+      <p>The professional members of our staff have 15 to  twenty years of experience in the different phases of clinical trials,  mastering excellence in both industry- and publicly-sponsored programs.</p>
+      <br />
+      <h3><strong>Principal Investigator:</strong> Dr. Isam A. Diab, M.D.</h3>
+      <p>Dr. Isam A. Diab specializes in rheumatology and  has been conducting clinical research for nearly 20 years as both a  Principal-Investigator and Sub-Investigator. Maintaining collaboration with  approximately 75 private practitioners in a large multi-disciplinary network,  Dr. Diab has grown his private practice into one of the largest within the  United States. His thorough understanding of medicine along with enthusiasm for  patient needs is clearly seen throughout his oversight of the clinical research  process.    </p>
+      <hr align="center" noshade="noshade" size=1 width="40%" />
+      <h3><strong>Sub-Investigator:</strong> Dr. Mirza I. Baig, M.D.</h3>
+      <p>Dr. Mirza I. Baig supports the clinical research  process in his role as Sub-Investigator. He maintains a vibrant practice of his  own in the area of physical medicine.</p>
+      <hr align="center" noshade="noshade" size=1 width="40%" />
+      <h3>Other Physicians</h3>
+      <p>Collaborating physicians contribute at various  levels to our clinical research business.   Some are acting Sub-Investigators helping to refer eligible patients  while others lend support through their expertise and hands on assistance. </p>
+      <p>&nbsp;</p>
+      <p><a href="otherStaff.php">Our nurses, study coordinators, and other staff</a></p>
+      <p>&nbsp;</p>
   </div>
-  <div class="content">
-    <h1>Meet Our Doctors</h1>
-    <hr align=left noshade="noshade" size=1 />
-    <p>The professional members of our staff have 15 to  twenty years of experience in the different phases of clinical trials,  mastering excellence in both industry- and publicly-sponsored programs.</p>
-    <br />
-    <h3><strong>Principal Investigator:</strong> Dr. Isam A. Diab, M.D.</h3>
-    <p>Dr. Isam A. Diab specializes in rheumatology and  has been conducting clinical research for nearly 20 years as both a  Principal-Investigator and Sub-Investigator. Maintaining collaboration with  approximately 75 private practitioners in a large multi-disciplinary network,  Dr. Diab has grown his private practice into one of the largest within the  United States. His thorough understanding of medicine along with enthusiasm for  patient needs is clearly seen throughout his oversight of the clinical research  process.    </p>
-    <hr align="center" noshade="noshade" size=1 width="40%" />
-    <h3><strong>Sub-Investigator:</strong> Dr. Mirza I. Baig, M.D.</h3>
-    <p>Dr. Mirza I. Baig supports the clinical research  process in his role as Sub-Investigator. He maintains a vibrant practice of his  own in the area of physical medicine.</p>
-    <hr align="center" noshade="noshade" size=1 width="40%" />
-    <h3>Other Physicians</h3>
-    <p>Collaborating physicians contribute at various  levels to our clinical research business.   Some are acting Sub-Investigators helping to refer eligible patients  while others lend support through their expertise and hands on assistance. </p>
-    <p>&nbsp;</p>
-    <p><a href="otherStaff.php">Our nurses, study coordinators, and other staff</a></p>
-    <p>&nbsp;</p>
-  </div>
-  <div class="footer"><center><span class="style4">Copyright Paramount Medical Research &amp; Consulting, LLC, 2010. <a href="../contact.php">Contact Us.</a></span></center></div>
+  <?php include($root . '/templates/footer.php'); ?>
   </div>
 <!-- end .container --></div>
 
--- dir/about.php
+++ dir/about.php
@@ -1,30 +1,30 @@
-  <?php
-    $root = $_SERVER['DOCUMENT_ROOT'];
-    include($root . '/templates/head.php');
-  ?>
+<?php
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  include($root . '/templates/head.php');
+?>
 
-  <body>
-    <div class="container">
-      <?php include($root . '/templates/header.php'); ?>
+<body>
+  <div class="container">
+    <?php include($root . '/templates/header.php'); ?>
 
-      <div id="bodybox">
-        <div id="content">
-          <h1>We're Here To Work For You.</h1>
-          <hr align=left noshade="noshade" size="1"/>
-          <p>Any  business can make a claim, but the team at Paramount Medical Research &amp;  Consulting, LLC believes in more than just words. We combine  expertise, persistence and attention to detail to support this claim. As  experienced professionals, we hold one another accountable by evaluating each  other&rsquo;s work and providing critical advice. Our staff is what makes the  difference. </p>
-          <p>&nbsp;</p>
-          <p>We strive to provide our study volunteers with respect, dignity and compassion.  They are not &ldquo;numbers&rdquo;, but rather partners in  our efforts. Our patient volunteers are priority and we aim to treat them as  such.  While participating in a research  study, our patients are provided ongoing information and education. They  consistently are given individualized attention and respect from all members of  our staff. We expect our seasoned staff of registered nurses, doctors and  support staff to deliver high quality, genuine care to each and every study volunteer.  This philosophy carries over into our study product. </p>
-          <p>We commit to our sponsors by providing quality data delivered with accuracy and  timeliness.  In other words, we take our  work seriously!</p>
+    <div id="bodybox">
+      <div id="content">
+        <h1>We're Here To Work For You.</h1>
+        <hr align=left noshade="noshade" size="1"/>
+        <p>Any  business can make a claim, but the team at Paramount Medical Research &amp;  Consulting, LLC believes in more than just words. We combine  expertise, persistence and attention to detail to support this claim. As  experienced professionals, we hold one another accountable by evaluating each  other&rsquo;s work and providing critical advice. Our staff is what makes the  difference. </p>
+        <p>&nbsp;</p>
+        <p>We strive to provide our study volunteers with respect, dignity and compassion.  They are not &ldquo;numbers&rdquo;, but rather partners in  our efforts. Our patient volunteers are priority and we aim to treat them as  such.  While participating in a research  study, our patients are provided ongoing information and education. They  consistently are given individualized attention and respect from all members of  our staff. We expect our seasoned staff of registered nurses, doctors and  support staff to deliver high quality, genuine care to each and every study volunteer.  This philosophy carries over into our study product. </p>
+        <p>We commit to our sponsors by providing quality data delivered with accuracy and  timeliness.  In other words, we take our  work seriously!</p>
 
-          <h3>Read more about us:</h3>
-          <ul class="nobullet">
-                <li> • <a href="About/history.php">The History of Paramount</a></li>
-                <li> • <a href="staff.php">Our Staff</a></li>
-          </ul>
-        </div>
-
-        <?php include($root . '/templates/footer.php'); ?>
+        <h3>Read more about us:</h3>
+        <ul class="nobullet">
+              <li> • <a href="About/history.php">The History of Paramount</a></li>
+              <li> • <a href="staff.php">Our Staff</a></li>
+        </ul>
       </div>
+
+      <?php include($root . '/templates/footer.php'); ?>
     </div>
-  </body>
+  </div>
+</body>
 </html>
