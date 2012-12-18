8dfc4d0964039234e1a1dc1576673c44f7468ae8
Omar Diab <odiab@stanford.edu>
2012-12-18 13:08:34 -0800

    forgot a few files

--- dir/About/otherStaff.php
+++ dir/About/otherStaff.php
@@ -1,50 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<title>Paramount Medical Research and Consulting</title>
-<link rel="stylesheet" href="/stylecss.php" media="screen" />
-</head>
+<?php
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  include($root . '/templates/head.php');
+?>
 
 <body>
-
-<table width="100%" height="50px" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td></td>
-  </tr>
-</table>
-
-<div class="container">
- 	<div class="headerlogo">
-      <table class="cellLink" width="100%" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-          <td height = 250px><a href="/index.php"></a></td>
-        </tr>
-      </table>
+  <div class="container">
+    <?php include($root . '/templates/header.php'); ?>
+    <div id="content">
+      <h1>About Us</h1>
+      <hr align="left" noshade="noshade" size=1 />
+      <h2>Other Staff at Paramount</h2>
+      <ul>
+        <li>Site Manager: Our Research Site Manager  is a Registered Nurse certified in clinical research and diabetes education  with over 15 years of experience coordinating, managing and consulting in the  clinical trial process. </li>
+        <li>Research Nurse Coordinators: Our main  Study Coordinators are Registered Nurses with combined experiences conducting  clinical trials including: NIH, industry- and investigator-initiated protocols,  pilot studies, infusion therapy, diabetes, hypertension, cardiology,  psychology, nephrology, neurology, and women’s health. </li>
+        <li>Study Coordinator: One member of our  coordinating team is a Syrian-licensed physician with seven years of research  coordination as well as Sub-Investigator. </li>
+        <li>Research Assistants: Our research  assistants are also certified medical assistants that have been trained in the  elements of clinical research, including GCP training. Our research assistants  play an integral role by supporting our research team during the clinical trial  process, including laboratory and administrative duties. </li>
+        <li>Consulting Services: We offer support  and direction to physicians interested in learning more about the clinical  research process.</li>
+        <li>GCP/Training: All members of our staff  are required to meet regulatory compliance guidelines and are expected to  maintain these on a yearly basis, at minimum. </li>
+      </ul>
+      <p>&nbsp;</p>
     </div>
-<!--navigation bar-->
-<? readfile("../../navigation.html"); ?>
-<!--navigation bar-->
-  <div class="sidebar1">
-<p>This website is still under construction.</p>
-  </div>
-  <div class="content">
-    <h1>About Us</h1>
-    <hr align="left" noshade="noshade" size=1 />
-    <h2>Other Staff
-    at Paramount</h2>
-    <ul>
-      <li>Site Manager: Our Research Site Manager  is a Registered Nurse certified in clinical research and diabetes education  with over 15 years of experience coordinating, managing and consulting in the  clinical trial process. </li>
-      <li>Research Nurse Coordinators: Our main  Study Coordinators are Registered Nurses with combined experiences conducting  clinical trials including: NIH, industry- and investigator-initiated protocols,  pilot studies, infusion therapy, diabetes, hypertension, cardiology,  psychology, nephrology, neurology, and women’s health. </li>
-      <li>Study Coordinator: One member of our  coordinating team is a Syrian-licensed physician with seven years of research  coordination as well as Sub-Investigator. </li>
-      <li>Research Assistants: Our research  assistants are also certified medical assistants that have been trained in the  elements of clinical research, including GCP training. Our research assistants  play an integral role by supporting our research team during the clinical trial  process, including laboratory and administrative duties. </li>
-      <li>Consulting Services: We offer support  and direction to physicians interested in learning more about the clinical  research process.</li>
-      <li>GCP/Training: All members of our staff  are required to meet regulatory compliance guidelines and are expected to  maintain these on a yearly basis, at minimum. </li>
-  </ul>
-    <p>&nbsp;</p>
-  <!-- end .content --></div>
-  <div class="footer"><center><span class="style4">Copyright Paramount Medical Research &amp; Consulting, LLC, 2010. <a href="../contact.php">Contact Us.</a></span></center></div>
+    <?php include($root . '/templates/footer.php'); ?>
   </div>
 <!-- end .container --></div>
 
