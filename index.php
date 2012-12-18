9c7564639b03f2dcd473626a7be1acfa35960315
Omar Diab <odiab@stanford.edu>
2012-12-18 10:41:12 -0800

    added head template, cleaned up html

--- index.php
+++ index.php
@@ -1,54 +1,50 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<title>Paramount Medical Research and Consulting</title>
-<link rel="stylesheet" href="style.css" media="screen" />
-</head>
-
+<?php 
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  include($root . '/templates/head.php');
+?>
 <body>
-
-<table width="100%" height="50px" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td></td>
-  </tr>
-</table>
-
-<div class="container">
- 	<div class="headerlogo">
+ <div class="container">
+    <div class="headerlogo">
       <table class="cellLink" width="100%" border="0" cellspacing="0" cellpadding="0">
         <tr>
           <td height = 250px><a href="index.html"></a></td>
         </tr>
       </table>
     </div>
-<!--navigation bar-->
-<? readfile("navigation.html"); ?>
-<!--navigation bar-->
-<div id="bodybox">
-  <div class="sidebar1">
-<p>This website is still under construction.</p>
+
+    <? readfile("navigation.html"); ?>
+
+    <div id="bodybox">
+      <div class="sidebar1">
+        <p>This website is still under construction.</p>
+      </div>
+      <div class="content">
+        <h1>We're Here to Work for You.</h1>
+        <hr align=left noshade="noshade" size=1 />
+        <p>
+          Paramount Medical Research &amp; Consulting, LLC is affiliated with Premier Physicians Centers – a Greater Cleveland-based multispecialty group with over 75 physicians in family practice, internal medicine, cardiology, endocrinology, rheumatology, oncology, orthopedic, and physical medicine.
+        </p>
+
+        <p>
+          Our participants are often referred by the various members of Premier Physicians, offering them the option of clinical research.
+        </p>
+
+        <hr align=center noshade="noshade" size=1 width=40% />
+
+        <h2>Mission</h2>
+        <p>
+          We strive to provide our study participants with altruistic care and compassion for the common goal of advancing science.  In doing so, we ensure that our participant volunteers consistently receive individualized care with respect and integrity.
+        </p>
+        <br />
+        <p>
+          We are committed to our sponsor partners in providing the research industry with the highest quality of results, including accurate and timely data, and enriched with the high standards of professionalism from our staff.  Our commitment to excellence is evident in each step of the clinical research process.
+        </p>
+      </div>
+
+      <div class="footer"><center><span class="style4">Copyright Paramount Medical Research &amp; Consulting, LLC, 2010. <a href="dir/contact.php">Contact Us.</a></span></center></div>
+      </div>
+    </div>
   </div>
-  <div class="content">
-    <h1>We're Here to Work for You.</h1>
-    <hr align=left noshade="noshade" size=1 />
-    <p>Paramount Medical Research &amp; Consulting, LLC is affiliated with Premier Physicians Centers – a Greater Cleveland-based multispecialty group with over 75 physicians in family practice, internal medicine, cardiology, endocrinology, rheumatology, oncology, orthopedic, and physical medicine.  </p>
-
-
-<p>Our participants are often referred by the various members of Premier Physicians, offering them the option of clinical research.</p>
-
-<hr align=center noshade="noshade" size=1 width=40% />
-
-<h2>Mission</h2>
-<p>We strive to provide our study participants with altruistic care and compassion for the common goal of advancing science.  In doing so, we ensure that our participant volunteers consistently receive individualized care with respect and integrity.</p>
-<br />
-<p>We are committed to our sponsor partners in providing the research industry with the highest quality of results, including accurate and timely data, and enriched with the high standards of professionalism from our staff.  Our commitment to excellence is evident in each step of the clinical research process.
-</p>
-  <!-- end .content --></div>
-  <div class="footer"><center><span class="style4">Copyright Paramount Medical Research &amp; Consulting, LLC, 2010. <a href="dir/contact.php">Contact Us.</a></span></center></div>
-  </div>
-<!-- end .container --></div>
 
 </body>
 </html>
