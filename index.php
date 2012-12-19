fa6bb3a61d4bbd5032bbda066ad455b0ea10e5ab
Omar Diab <odiab@stanford.edu>
2012-12-19 14:19:39 -0800

    added standard hr style, made rearranged index content

--- index.php
+++ index.php
@@ -10,18 +10,6 @@
     <div id="bodybox">
       <div id="content">
         <h1>We're Here to Work for You.</h1>
-        <hr align=left noshade="noshade" size=1 />
-        <p>
-          Paramount Medical Research &amp; Consulting, LLC is affiliated with Premier Physicians Centers – a Greater Cleveland-based multispecialty group with over 75 physicians in family practice, internal medicine, cardiology, endocrinology, rheumatology, oncology, orthopedic, and physical medicine.
-        </p>
-
-        <p>
-          Our participants are often referred by the various members of Premier Physicians, offering them the option of clinical research.
-        </p>
-
-        <hr align=center noshade="noshade" size=1 width=40% />
-
-        <h2>Mission</h2>
         <p>
           We strive to provide our study participants with altruistic care and compassion for the common goal of advancing science.  In doing so, we ensure that our participant volunteers consistently receive individualized care with respect and integrity.
         </p>
@@ -29,8 +17,17 @@
         <p>
           We are committed to our sponsor partners in providing the research industry with the highest quality of results, including accurate and timely data, and enriched with the high standards of professionalism from our staff.  Our commitment to excellence is evident in each step of the clinical research process.
         </p>
-      </div>
 
+        <hr />
+
+        <p>
+          Paramount Medical Research &amp; Consulting, LLC is affiliated with Premier Physicians Centers – a Greater Cleveland-based multispecialty group with over 75 physicians in family practice, internal medicine, cardiology, endocrinology, rheumatology, oncology, orthopedic, and physical medicine.
+        </p>
+
+        <p>
+          Our participants are often referred by the various members of Premier Physicians, offering them the option of clinical research.
+        </p>
+      </div>
       <?php include($root . '/templates/footer.php'); ?>
     </div>
   </div>
