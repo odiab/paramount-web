6a982a9ac1c7d11f4d6475035e945f2b668c8404
Omar Diab <odiab@stanford.edu>
2012-12-20 15:56:45 -0800

    Renamed headers

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,8 +1,8 @@
 <ul id="nav">
         <li><a href="/">Home</a></li>
         <li><a href="/pages/staff.php">Meet Our Staff</a></li>
-        <li><a href="/pages/participants.php">Potential Participants</a></li>
-        <li><a href="/pages/sponsors.php">Sponsors</a></li>
+        <li><a href="/pages/participants.php">For Participants</a></li>
+        <li><a href="/pages/sponsors.php">For Sponsors</a></li>
         <li><a href="/pages/locations.php">Locations</a></li>
         <li><a href="/pages/contact.php">Contact Us</a></li>
 </ul>
