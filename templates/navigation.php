9e6043e2163a2fef37e75e1a953b969d5d924527
Omar Diab <odiab@stanford.edu>
2012-12-20 15:52:26 -0800

    rearranged participation info

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,7 +1,6 @@
 <ul id="nav">
         <li><a href="/">Home</a></li>
         <li><a href="/pages/staff.php">Meet Our Staff</a></li>
-        <li><a href="/pages/ourStudies.php">Our Studies</a></li>
         <li><a href="/pages/participants.php">Potential Participants</a></li>
         <li><a href="/pages/sponsors.php">Sponsors</a></li>
         <li><a href="/pages/locations.php">Locations</a></li>
