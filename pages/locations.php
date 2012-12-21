8e6e1077ae8a9f90e26ed82c9e709c97b0eaf152
Omar Diab <o.s.diab@gmail.com>
2012-12-20 16:17:15 -0800

    Made locations use cards, cleaned up a couple more pages

--- pages/locations.php
+++ pages/locations.php
@@ -1,37 +1,41 @@
 <?php
-  $root = $_SERVER['DOCUMENT_ROOT'];
-  include($root . '/templates/head.php');
+$styles = array('contacts.css');
+$root = $_SERVER['DOCUMENT_ROOT'];
+include($root . '/templates/head.php');
+include($root . '/helpers/contactHelpers.php');
 ?>
 
 <body>
+  <div id="bodyContainer">
+    <?php include($root . '/templates/header.php'); ?>
 
-<div id="bodyContainer">
-  <?php include($root . '/templates/header.php'); ?>
+    <div id="bodybox">
+      <div id="content">
+        <h1>Site Locations</h1>
 
-  <div id="bodybox">
-    <div id="content">
-      <h1>Site Locations</h1>
-      <hr align="left" noshade="noshade" size=1 />
-
-      <h2>Cleveland’s  Westside:</h2>
-      <h3>Dr.  Isam A. Diab</h3>
-      <p>Conveniently  located just minutes from Cleveland Hopkins International Airport.</p>
-      <p>&nbsp;</p>
-      <p>18660  Bagley Road, Phase ll, Suite 102B</p>
-      <p>Middleburg  Heights, Ohio 444130</p>
-      <p>Phone: 440-826-0742</p>
-      <p>Fax:  440-891-1576</p>
-      <p>&nbsp;</p>
-
-      <h2>Cleveland’s  Eastside:</h2>
-      <h3>Dr.  Anne M. Carrol</h3>
-      <p>Located  in beautiful Beachwood, Ohio with ample access to the best restaurants, hotels  and shopping.</p>
-      <p>&nbsp;</p>
-      <p>23250  Chagrin Boulevard, Commerce Park Five, Suite 201</p>
-      <p>Beachwood,  Ohio 44122</p>
-      <p>Phone:  216-504-8888</p>
-      <p>Fax:  216-504-8887</p>
-      <p>&nbsp;</p>
+        <?php
+          $sites = array();
+          $sites[] = array(
+            'title' => "Cleveland's Westside",
+            'name' => 'Dr. Isam A. Diab',
+            'suffix' => 'M.D.',
+            'description' => 'Conveniently located just minutes from Cleveland Hopkins International Airport.',
+            'phone' => 4408260742,
+            'fax' => 4408911576,
+            'address' => "18660 Bagley Road\nPhase 11, Suite102B\nMiddleburg Heights, OH 44130",
+          );
+          $sites[] = array(
+            'title' => "Cleveland's Eastside",
+            'name' => 'Dr. Anne M. Carrol',
+            'suffix' => 'M.D.',
+            'description' => 'Located in beautiful Beachwood, Ohio with ample access to the best restaurants, hotels and shopping.',
+            'phone' => 2165048888,
+            'fax' => 2165048887,
+            'address' => "23250 Chagrin Boulevard\nCommerce Park Five, Suite 201\nBeachwood, OH 44122",
+          );
+          include($root . '/templates/locationPanes.php');
+        ?>
+      </div>
     </div>
 
     <?php include($root . '/templates/footer.php'); ?>
--- pages/participants/participate.php
+++ pages/participants/participate.php
@@ -10,9 +10,7 @@
     <div id="bodybox">
       <div id="content">
         <h1>Potential Participants</h1>
-        <hr align="left" noshade="noshade" size=1 /><h2>Why Participate in a Clinical Trial?</h2></p>
-        <p>Clinical research participants can volunteer to  participate in a trial or be invited by the research team to consider the  option of getting involved in a research trial. Participants have largely  benefitted from the one-on-one attention. Many choose to participate in order  to gain more information about his/her particular condition. Some studies have  shown that those who choose to participate in a study do better than their  counterparts, despite the treatment arm of the protocol. Still others find that  contributing to the research process gives them a sense of helping others, as  well as future generations. </p>
-        <p>&nbsp;</p>
+        <p>Clinical research participants can volunteer to  participate in a trial or be invited by the research team to consider the  option of getting involved in a research trial. Participants have largely  benefitted from the one-on-one attention. Many choose to participate in order  to gain more information about his/her particular condition. Some studies have  shown that those who choose to participate in a study do better than their  counterparts, despite the treatment arm of the protocol. Still others find that  contributing to the research process gives them a sense of helping others, as  well as future generations.</p>
       </div>
       <?php include($root . '/templates/footer.php'); ?>
     </div>
