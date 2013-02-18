b04cd028738b9b214a6f93807c579fc397da8db3
Omar Diab <o.s.diab@gmail.com>
2013-02-17 22:10:59 -0700

    changed copyright to symbol

--- www/templates/footer.php
+++ www/templates/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
   <p>
-    Copyright Paramount Medical Research &amp; Consulting, LLC, 2010.
+    &copy; Paramount Medical Research &amp; Consulting, LLC, 2010.
     <a href="/?page=contact">Contact Us.</a>
     |
     <a href="/?page=aboutSite">About this site</a>
