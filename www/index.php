c788001b37702f3692974a54dbf7498a2e14bc73
Omar Diab <o.s.diab@gmail.com>
2013-07-31 00:34:09 -0700

    moved html tags into index.php

--- www/index.php
+++ www/index.php
@@ -1,23 +1,25 @@
-<?php
-  include('Assets.php');
-  Assets::load(TEMPLATE, 'head');
-  $path = $_SERVER['REQUEST_URI'];
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <?php
+    include('Assets.php');
+    Assets::load(TEMPLATE, 'head');
+    $path = $_SERVER['REQUEST_URI'];
+  ?>
 
-<body>
-  <div id="bodyContainer">
-    <?php Assets::load(TEMPLATE, 'header', array('path' => $path)); ?>
-    <div id="bodybox">
-      <div id="content">
-        <?php
-        if (Assets::load(PAGE, $path) != 0) {
-          Assets::load(PAGE, '404');
-        }
-        ?>
+  <body>
+    <div id="bodyContainer">
+      <?php Assets::load(TEMPLATE, 'header', array('path' => $path)); ?>
+      <div id="bodybox">
+        <div id="content">
+          <?php
+          if (Assets::load(PAGE, $path) != 0) {
+            Assets::load(PAGE, '404');
+          }
+          ?>
+        </div>
+        <?php Assets::load(TEMPLATE, 'footer'); ?>
       </div>
-      <?php Assets::load(TEMPLATE, 'footer'); ?>
     </div>
-  </div>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-</body>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  </body>
 </html>
